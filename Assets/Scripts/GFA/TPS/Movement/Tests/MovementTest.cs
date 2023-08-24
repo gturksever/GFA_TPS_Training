046f8f4ac8556d6f387d5ca13612dbd7889028af
gturksever <gsgturk@hotmail.com>
2023-08-24 04:45:40 +0300

    First_Commit

---
+++ Assets/Scripts/GFA/TPS/Movement/Tests/MovementTest.cs
@@ -0,0 +1,55 @@
+using GFA.TPS.Input;
+using GFA.TPS.Movement;
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace GFA.TPS.Test
+{
+    public class MovementTest : MonoBehaviour
+    {
+        [SerializeField]
+        private Vector2 _movementInput;
+
+        [SerializeField]
+        private CharacterMovement _characterMovement;
+
+
+        [SerializeField]
+        private Vector3 _externalForceValue;
+
+        private GameInput _gameInput;
+
+        private void Awake()
+        {
+            _gameInput = new GameInput();
+        }
+
+        private void OnEnable()
+        {
+            _gameInput.Enable();
+            _gameInput.Player.Dodge.performed += OnDodgeButtonPressed;
+        }
+
+        private void OnDisable()
+        {
+            _gameInput.Disable();
+
+            _gameInput.Player.Dodge.performed -= OnDodgeButtonPressed;
+
+        }
+
+        private void OnDodgeButtonPressed(InputAction.CallbackContext obj)
+        {
+            _characterMovement.ExternalForces += _externalForceValue;
+        }
+
+        private void Update()
+        {
+           var input = _gameInput.Player.Movement.ReadValue<Vector2>();
+            _characterMovement.MovementInput = input;
+        }
+    }
+
+}
+
